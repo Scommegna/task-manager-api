c9a65b4ffd65bb96877e4bc49632fb9595630574
Lucas Scommegna <scommegnal@hotmail.com>
2023-07-20 14:34:42 -0300

    made route and controllers base structure

---
+++ controllers/tasks.js
@@ -0,0 +1,24 @@
+const getAllTasks = (req, res) => {
+  res.send("all items");
+};
+
+const createTask = (req, res) => {
+  res.send("all items");
+};
+const getTask = (req, res) => {
+  res.send("all items");
+};
+const updateTask = (req, res) => {
+  res.send("all items");
+};
+const deleteTask = (req, res) => {
+  res.send("all items");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
