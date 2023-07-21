386bf149bf1735f01a1a7282eab15d4602df2acd
Lucas Scommegna <scommegnal@hotmail.com>
2023-07-21 14:05:41 -0300

    not found route

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middlewares/not-found");
+
 // Middlewares
+app.use(express.static("./public"));
 app.use(express.json());
 
 // Routes
-app.get("/hello", (req, res) => {
-  res.send("Task manager app");
-});
-
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
 
 const port = 3000;
 
